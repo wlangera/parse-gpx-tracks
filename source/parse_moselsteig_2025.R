528fc560994d72466eecc8e092a274350bac2cb1
Ward Langeraert <ward.langeraert@inbo.be>
2025-08-16 15:04:49 +0200

    calculate elevation

--- source/parse_moselsteig_2025.R
+++ source/parse_moselsteig_2025.R
@@ -1,17 +1,25 @@
 library(sf)
-source(file.path("source", "R", "merge_gpx_tracks.R"))
+library(readr)
+
+lapply(list.files(file.path("source", "R"), full.names = TRUE), source)
+
 
 # Data sources
 data_path <- file.path("data", "moselsteig_2025")
 
 # Read track data
-track_data <- read_delim(
+track_data_raw <- read_delim(
   file.path(data_path, "track_data.csv"),
   col_types = cols(date = col_date("%d/%m/%Y")),
   show_col_types = FALSE
 )
 
 # Calculate ascent and descent
+elevation_df <- calculate_elevation(data_path, quiet = TRUE)
+
+# Add elevation to track_data
+track_data <- track_data_raw %>%
+  left_join(elevation_df, by = join_by(gpx_file))
 
 # Merge gpx files by date
 sf_tracks <- merge_gpx_tracks(
@@ -23,5 +31,5 @@ sf_tracks <- merge_gpx_tracks(
 # Check
 mapview::mapview(sf_tracks, zcol = "label", layer = "Legende")
 
-st_write(sf_tracks, file.path("output", "tracks_czech_republic_2025.gpkg"),
+st_write(sf_tracks, file.path("output", "tracks_moselsteig_2025.gpkg"),
          append = FALSE)
