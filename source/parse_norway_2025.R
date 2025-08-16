5a9dc70d16ad29bee1c33e8ca20965bd3d9b27e6
Ward Langeraert <ward.langeraert@inbo.be>
2025-08-16 15:22:26 +0200

    norway parsing

---
+++ source/parse_norway_2025.R
@@ -0,0 +1,28 @@
+library(sf)
+library(readr)
+
+lapply(list.files(file.path("source", "R"), full.names = TRUE), source)
+
+
+# Data sources
+data_path <- file.path("data", "norway_2025")
+
+# Read track data
+track_data <- read_delim(
+  file.path(data_path, "track_data.csv"),
+  col_types = cols(date = col_date("%d/%m/%Y")),
+  show_col_types = FALSE
+)
+
+# Merge gpx files by date
+sf_tracks <- merge_gpx_tracks(
+  data_path,
+  track_data,
+  quiet = TRUE
+)
+
+# Check
+mapview::mapview(sf_tracks, zcol = "label", layer = "Legende")
+
+st_write(sf_tracks, file.path("output", "tracks_norway_2025.gpkg"),
+         append = FALSE)
