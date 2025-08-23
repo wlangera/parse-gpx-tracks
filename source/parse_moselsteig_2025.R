d0bc930c82c5fd33d1808efb5b939d1730b82b3b
Ward Langeraert <ward.langeraert@inbo.be>
2025-08-23 11:48:31 +0200

    create nice plot

--- source/parse_moselsteig_2025.R
+++ source/parse_moselsteig_2025.R
@@ -31,5 +31,74 @@ sf_tracks <- process_gpx_activities(
 # Check
 mapview::mapview(sf_tracks, zcol = "label", layer = "Legende")
 
+library(ggmap)
+# --- Flexible bbox with separate buffer ---
+# Make bbox with buffer in meters
+make_bbox_buffer <- function(sf_obj, lon_buffer_m = 1000, lat_buffer_m = 1000) {
+  # Ensure WGS84
+  sf_obj <- st_transform(sf_obj, 4326)
+  
+  # Find suitable UTM zone from centroid
+  centroid <- st_centroid(st_union(sf_obj))
+  lon <- st_coordinates(centroid)[1]
+  utm_zone <- floor((lon + 180) / 6) + 1
+  crs_utm <- paste0("+proj=utm +zone=", utm_zone, " +datum=WGS84 +units=m +no_defs")
+  
+  # Transform to UTM
+  sf_utm <- st_transform(sf_obj, crs_utm)
+  bbox <- st_bbox(sf_utm)
+  
+  # Apply buffer in meters
+  bbox_exp <- c(
+    xmin   = bbox[["xmin"]] - lon_buffer_m,
+    ymin = bbox[["ymin"]] - lat_buffer_m,
+    xmax  = bbox[["xmax"]] + lon_buffer_m,
+    ymax    = bbox[["ymax"]] + lat_buffer_m
+  )
+  
+  # Convert back to WGS84
+  bbox_poly <- st_as_sfc(st_bbox(bbox_exp, crs = st_crs(sf_utm)))
+  bbox_poly_wgs84 <- st_transform(bbox_poly, 4326)
+  out_box <- st_bbox(bbox_poly_wgs84)
+  names(out_box) <- c("left", "bottom", "right", "top")
+  
+  return(out_box)
+}
+
+# Example: 0.05° lon buffer, 0.02° lat buffer
+# Download Stamen/OSM basemap
+basemap <- get_stadiamap(
+  bbox = make_bbox_buffer(sf_tracks, 5000, 1000),
+  zoom = 12
+)
+
+sf_tracks <- sf_tracks %>%
+  mutate(geometry = st_cast(geometry, "MULTILINESTRING"))
+
+# Base plot
+ggmap(basemap) +
+  geom_sf(
+    data = sf_tracks,
+    aes(color = label),
+    linewidth = 1,
+    inherit.aes = FALSE
+  ) +
+  scale_color_viridis_d(name = "Day") +
+  guides(color = guide_legend(override.aes = list(linewidth = 2))) +
+  ggspatial::annotation_scale(location = "br", style = "ticks",
+                              width_hint = 0.4, line_width = 3, text_cex = 1) +
+  ggspatial::annotation_north_arrow(
+    location = "tl", which_north = "true",
+    style = north_arrow_fancy_orienteering
+  ) +
+  theme(
+    legend.position = "inside",
+    legend.position.inside = c(0.98, 0.1),
+    legend.justification = c(1, 0),
+    axis.title = element_blank(),
+    axis.text = element_blank(),
+    axis.ticks = element_blank(),
+    legend.background = element_rect(fill = alpha("white", 0.75)))
+
 st_write(sf_tracks, file.path("output", "tracks_moselsteig_2025.gpkg"),
          append = FALSE)
